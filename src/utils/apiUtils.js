0339b9dcc87c1225a4c9e5455dba8f9d4eb818ef
NguyenPhi <nguyenphidhbk@gmail.com>
2022-02-07 09:18:33 +0700

    update

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://movienew.cybersoft.edu.vn/api/",
-})
+  baseURL: "https://movienew.cybersoft.edu.vn/api/",
+});
 api.interceptors.request.use((config) => {
-    config.headers = {
-        TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyMCIsIkhldEhhblN0cmluZyI6IjE3LzA3LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1ODAxNjAwMDAwMCIsIm5iZiI6MTYyNjcxNDAwMCwiZXhwIjoxNjU4MTYzNjAwfQ.CyAnnc8e2Rp7YmuJCdtEj-Wp7RvlDenB9Dad6NV0R20", 
-    };
-    return config;
+  config.headers = {
+    TokenCybersoft:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyMCIsIkhldEhhblN0cmluZyI6IjE3LzA3LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1ODAxNjAwMDAwMCIsIm5iZiI6MTYyNjcxNDAwMCwiZXhwIjoxNjU4MTYzNjAwfQ.CyAnnc8e2Rp7YmuJCdtEj-Wp7RvlDenB9Dad6NV0R20",
+    Authorization: localStorage.getItem("UserAdmin")
+      ? `Bearer ${JSON.parse(localStorage.getItem("UserAdmin")).accessToken}`
+      : "",
+  };
+  return config;
 });
